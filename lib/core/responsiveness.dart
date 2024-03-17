2fb9651637853742416885b998452b076247ed9f
Jaineel Mamtora <jaineelmamtora99@gmail.com>
2024-03-17 21:32:01 +0530

    deployment-one

--- lib/core/responsiveness.dart
+++
@@ -1,6 +0,0 @@
-abstract class Insets {
-  static const double scale = 1.0;
-  static double get small => 8 * scale;
-  static double get medium => 12 * scale;
-  static double get large => 16 * scale;
-}
